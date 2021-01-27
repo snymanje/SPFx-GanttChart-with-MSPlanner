b38de19a9ba016d9415d723d3ba0ee72b9041b73
snymanje <jean.snyman@gmail.com>
2021-01-27 11:30:20 +0200

    added loading status

--- src/webparts/projectManagement/components/GanttChart/GanttChart.tsx
+++ src/webparts/projectManagement/components/GanttChart/GanttChart.tsx
@@ -1,10 +1,16 @@
 import * as React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Spinner from 'react-bootstrap/Spinner'
+import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
 
 import Chartt from "./chart";
 import { GetPlannerdata } from "../../utils/getPlannerData";
 
 const Chart = () => {
   const [data, setData] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
 
   let today: Date = new Date();
   const day = 1000 * 60 * 60 * 24;
@@ -18,19 +24,58 @@ const Chart = () => {
   React.useEffect(() => {
     // Self invoking async function
     (async () => {
-      const plannerData = await GetPlannerdata(
-        "eIgWi8Rmw0-DKaO6-AMaZ2UAHW3O",
-        data,
-        day,
-        today.getTime()
-      );
-      setData(plannerData);
+      try {
+        // throw line below is for testing purposes only.
+        //throw new Error("Error occured")
+        const plannerData = await GetPlannerdata(
+          "eIgWi8Rmw0-DKaO6-AMaZ2UAHW3O",
+          data,
+          day,
+          today.getTime()
+        );
+        setData(plannerData);
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+        console.log(err.message);
+        setLoading(false);
+      }
     })();
   }, []);
 
   return (
     <div>
-      <Chartt data={data} day={day} today={today.getTime()} title={"Intelligent Automation Project Management"} />
+      {error ? (
+        <div className="d-flex justify-content-center">
+          <Alert variant="danger">
+            <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+            <p>
+              {error}
+            </p>
+          </Alert>
+        </div>
+      ) : loading ? (
+        <div className="d-flex justify-content-center">
+          <Button variant="light" disabled>
+            <Spinner
+              as="span"
+              animation="grow"
+              size="sm"
+              role="status"
+              aria-hidden="true"
+              className="mr-2"
+            />
+            Loading data from Planner...please wait.
+          </Button>
+        </div>
+      ) : (
+        <Chartt
+          data={data}
+          day={day}
+          today={today.getTime()}
+          title={"Intelligent Automation Project Management"}
+        />
+      )}
     </div>
   );
 };
